be9cd6d673dc2f9a99221c4bcf2845779982922e
Ravi Kumar Patel <ravikumarpatel2001@gmail.com>
2025-04-17 15:21:48 +0530

    reverse a string II leetcode problem(#541) solution

---
+++ src/ReverseStringII.java
@@ -0,0 +1,31 @@
+public class ReverseStringII {
+    public static String reverseString(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+    public static String makeSubstring(String str, int left, int right) {
+        right = Math.min(right, str.length());
+        return str.substring(left, right);
+    }
+    public static String reverseStr(String s, int k) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i += 2 * k) {
+            String segment = makeSubstring(s, i, i + 2 * k);
+
+            if (segment.length() >= k) {
+                String reversed = reverseString(segment.substring(0, k));
+                result.append(reversed).append(segment.substring(k));
+            }else {
+                result.append(reverseString(segment));
+            }
+        }
+
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        String string = "abcdefg";
+        int k = 2;
+
+        System.out.println("Reversed String: " + reverseStr(string, k));
+    }
+}
