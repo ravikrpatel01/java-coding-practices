a58ac62b1dc3564c29d266c58a1a84673127e78a
Ravi Kumar Patel <ravikumarpatel2001@gmail.com>
2025-05-12 23:21:36 +0530

    tower of hanoi problem

--- src/recursion/TowerOfHanoiProblem.java
+++ src/recursion/TowerOfHanoiProblem.java
@@ -1,7 +1,18 @@
 package recursion;
 
 public class TowerOfHanoiProblem {
-    public static void main(String[] args) {
+    public static void towerOfHanoi(int n, char source, char helper, char destination) {
+        if (n == 1) {
+            System.out.println("MOVE disk " + n + " from " + source + " to " + destination);
+            return;
+        }
 
+        towerOfHanoi(n-1, source, destination, helper);
+        System.out.println("MOVE disk " + n + " from " + source + " to " + destination);
+        towerOfHanoi(n-1, helper, source, destination);
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        towerOfHanoi(n, 'A', 'B', 'C');
     }
 }
